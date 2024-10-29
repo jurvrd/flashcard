ee078bbfeb0a084f1c2235677a0d9dbce2db5104
ju <147503818+jurvrd@users.noreply.github.com>
2024-10-29 08:35:21 -0300

    gf

--- perunta.js
+++ perunta.js
@@ -3,5 +3,22 @@ criaCartao(
   "Participou de uma experiencia criada pelos militares."
   )
 criaCartao(
-  
+  "Como Tony vira o Homem de Ferro?",
+  "Ele é sequestrado e cria uma roupa tecnológica no alojamento."
+)
+criaCartao(
+  "Qual é o nome do martelo do Thor?",
+  "Mjolnir."
+)
+criaCartao(
+  "Sobre qual cidade Hawkeye e Viuva Negra costumam lembrar?",
+  "Budapeste."
+)
+criaCartao(
+  "Quantas joias do infinito existem?",
+  "6."
+)
+criaCartao(
+  "Qual é a raça alienigena que Loki envia para invadir a terra em Os Vingadores?",
+  "Os Chitauri"
 )
